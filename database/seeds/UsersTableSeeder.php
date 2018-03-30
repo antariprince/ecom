63e916605ecde15af33bf0ab6a186ac2d64487eb
teejay <teejay.hidalgo@gmail.com>
2018-03-30 11:34:10 +0800

    update user seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,17 +14,18 @@ class UsersTableSeeder extends Seeder
         App\User::create([
         	'name' => 'admin',
         	'password' => bcrypt('admin'),
-        	'email' => 'tj.module.test@gmail.com',
+        	'email' => 'admin@gmail.com',
         	'admin' => 1,
         	'avatar' => asset('uploads/avatars/screenshot.jpg')
         ]);
 
         App\User::create([
             'name' => 'sheldon cooper',
-            'password' => bcrypt('password'),
+            'password' => bcrypt('admin'),
             'admin' => 1,
             'email' => 'tj.module.test2@gmail.com',
             'avatar' => asset('uploads/avatars/screenshot.jpg')
         ]);
+
     }
 }
